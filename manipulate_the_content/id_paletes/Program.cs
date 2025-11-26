d987800e7fc567451aaf42131a5d65ac6f3ba056
Tamires Lima CS <80965417+TaTTA-23@users.noreply.github.com>
2025-11-26 15:26:48 -0300

    Redimensione a matriz para remover elementos.
    Aqui, você está chamando o Resize()método passando o palletsarray por referência, usando a refpalavra-chave `reference`. Em alguns casos, os métodos exigem que você passe argumentos por valor (o padrão) ou por referência (usando a palavra-chave `ref`). Os motivos pelos quais isso é necessário requerem uma explicação longa e complexa sobre como os objetos são gerenciados no .NET.

--- manipulate_the_content/id_paletes/Program.cs
+++ manipulate_the_content/id_paletes/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string[] pallets = [ "B14", "A11", "B12", "A13" ];
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
